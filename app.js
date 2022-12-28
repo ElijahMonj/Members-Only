ca1bc4fad4bd6e76688314c4f0f9bb8f45c3ebd4
Elijah Monjardin <monjardinelijah120@gmail.com>
2022-12-28 18:37:13 +0800

    added delete

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const User = mongoose.model(
   new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true },
     isAdmin: { type: Boolean, required: true }
   })
 );
@@ -35,7 +36,7 @@ app.get("/", (req, res) => {
   async function getValueAsync() {
     let coll = mongoose.connection.db.collection("messages");
     let data = await coll.find({}, {limit:10, sort:{name:-1}}).toArray();
-    console.log(data)
+    //console.log(data)
     res.render("index", { user: req.user, messages: data });
   }
   getValueAsync()
@@ -82,13 +83,12 @@ app.post("/sign-up",[
   const user = new User({
     username: req.body.username,
     password: req.body.password,
+    isMember:false,
     isAdmin: x,
   }).save(err => {
     if (err) { 
       return next(err);
     }
-    
-    
     res.redirect("/");
   });
   }
@@ -133,7 +133,36 @@ app.post(
 );
 //to create a log in post
 
-
+app.post('/delete', (req, res) => {
+  console.log(req.body.usr) 
+  var MongoClient = require('mongodb').MongoClient;
+  let client=new MongoClient(mongoDb);
+  qwer=`ObjectID("${req.body.usr}")`
+  console.log(qwer);
+  const  ObjectID = require('mongodb').ObjectId;
+  async function run() {
+    try {
+      const database = client.db("test");
+      const movies = database.collection("messages");
+      // Query for a movie that has title "Annie Hall"
+      const query = { _id: ObjectID(req.body.usr) };
+      const result = await movies.deleteOne(query);
+      if (result.deletedCount === 1) {
+        console.log("Successfully deleted one document.");
+      } else {
+        console.log("No documents matched the query. Deleted 0 documents.");
+        
+      }
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+  setTimeout(refresh, 500);
+  function refresh(){
+    res.redirect('/')
+  }
+})
 //TEST----
 app.post('/new', function(req,res){
   
@@ -158,6 +187,7 @@ MongoClient.connect(mongoDb, function(err, db) {
   });
 });
 async function getValueAsync() {
+  
   let coll = mongoose.connection.db.collection("messages");
   let data = await coll.find({}, {limit:10, sort:{name:-1}}).toArray();
   console.log("-------------------------")
@@ -165,7 +195,10 @@ async function getValueAsync() {
   console.log("-------------------------")
 }
 getValueAsync()
-  res.redirect('/')
+setTimeout(refresh, 500);
+  function refresh(){
+    res.redirect('/')
+  }
 })
 //TEST----
-app.listen(3001, () => console.log("app listening on port 3000!"));
+app.listen(3000, () => console.log("app listening on port 3000!"));
